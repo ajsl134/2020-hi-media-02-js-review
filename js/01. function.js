5edfde8e5211dac096089d503d56f992f447d76b
ajsl134 <ajsl134@naver.com>
2020-08-28 18:13:49 +0900

    third commit

--- js/01. function.js
+++ js/01. function.js
@@ -1,50 +1,142 @@
-    $("#bt1").click(function(){
-        $("#img1").stop().fadeIn(500);
-    });
-    $("#bt2").click(function(){
-        $("#img1").stop().fadeOut(500);
-    });
-    $("#bt3").click(function(){
-        $("#img1").toggle();
-    });
-    $("#bt4").click(function(){
-        $("#img1").stop().fadeToggle(500);
-    });
-    $("#bt5").click(function(){
-        $("#img1").stop().slideUp(500);
-    });
-    $("#bt6").click(function(){
-        $("#img1").stop().slideDown(500);
-    });
-    $("#bt7").click(function(){
-        $("#img1").stop().slideToggle(500);        
-    });
+$("#bt1").click(function(){
+	$("#img1").stop().fadeOut(500);
+});
+$("#bt2").click(function(){
+	$("#img1").stop().fadeIn(500);
+});
+$("#bt3").click(function(){
+	$("#img1").toggle();
+});
+$("#bt4").click(function(){
+	$("#img1").stop().fadeToggle(500);
+});
+$("#bt5").click(function(){
+	$("#img1").stop().slideUp(500);
+});
+$("#bt6").click(function(){
+	$("#img1").stop().slideDown(500);
+});
+$("#bt7").click(function(){
+	$("#img1").stop().slideToggle(500);
+});
 
-    /******************** Vanila Script / ES5 ***********************/
-    // 아래 제이쿼리 객체 //
-    document.getElementById("bt8"); // $("#bt8")
-    document.getElementsByClassName("btn"); // $(".btn")
-    document.getElementsByTagName("button"); // $("button")
-    // 아래 자바스크립트 객체 //
-    document.querySelector("#bt8");
-    document.querySelector(".btn");
-    document.querySelector("button");
-    // 제이쿼리는 대부분이 함수!! $ = jQurey
-    
-    
-    document.querySelector("#bt8").addEventListener("click", function(){
-        document.querySelector("#img2").style.display="none";
-    });
+/************** Vanila Script / ES5 *****************/
+document.getElementById("bt8");	// $("#bt8")
+document.getElementsByClassName("btn");	// $(".btn")
+document.getElementsByTagName("button");	// $("button")
+document.querySelector("#bt8");
+document.querySelector(".btn");
+document.querySelector("button");
 
-    document.querySelector("#bt9").addEventListener("click", function(){
-        document.querySelector("#img2").style.display="";
-    });    
+console.log(	$("#bt8")[0]	);
+console.log(	$(document.getElementById("bt8"))	);
 
-/*     var document = {
-        querySelector: function(str){
-            return this.getElementById(str);
-        }
-        style: {
-            display: 'block'
-        }
-    } */
+document.querySelector("#bt8").addEventListener("click", function(){
+	document.querySelector("#img2").style.display = "none";
+});
+
+document.querySelector("#bt9").addEventListener("click", function(){
+	document.querySelector("#img2").style.display = "";
+});
+
+/*
+var document = {
+	querySelector: function(str){
+		return this.getElementById(str);
+	},
+	style: {
+		display: 'block'
+	}
+}
+var a = ['A', 'B'];
+var b = {
+	x: 10,
+	y: 20
+};
+console.log(b.x);
+console.log(b.y);
+*/
+
+
+/********** 변수, 함수, 배열, 객체 **********/
+/********** 변수(variable) **********/
+//var a = '?'
+// primittive type : 숫자(Number), 문자(String), 불린(Boolean), null, undefined
+// reference type : 배열(Array), 객체(Object)
+
+var a; // undefined
+if(a) console.log(a);
+
+var a = 1; //숫자
+if(a) console.log(a);
+
+var a = '1'; //문자
+if(a) console.log(a);
+
+var a = true; 
+if(a) console.log(a);
+
+var a = null; // 빈값
+if(a) console.log(a);
+
+// false == 0, '', null, undefined
+if(a) {
+    console.log("Hi~")
+}
+
+console.clear();
+
+/********** 함수(function) **********/
+
+// 함수선언문 - Hoisting(끌어올리다.)
+b();
+function b(){
+    console.log('B');
+}
+
+// 함수표현식
+var c = function(){
+    console.log('C');
+}
+c();
+
+// 함수의 인자(Arguments)
+function d(x,y){
+   x * y;
+}
+
+function sum(x, y, cb) {
+    var hap = x + y;
+    cb(hap);
+}
+
+sum(10, 20, function(r){
+    alert("10 더하기 20은 " + r + "입니다.")
+});
+
+/* var jQuery = function(str){
+    this = new Array();
+    this[0] = document.querySelector(str);
+    this.prototype.click = function(fn){
+        fn();
+    }
+    return this;
+} */
+
+
+//움직여, 원위치
+// $("객체").stop().animate({}, 1000, "swing", function(){});
+$("#bt10").click(function(){
+    $("#img3").stop().animate({"left": "800px"}, 2000, function(){
+        $(this).css("opacity", 0.5);
+    })
+});
+
+$("#bt11").click(function(){
+    $("#img3").stop().animate({"left": "0"}, 1000)
+});
+
+reservacation('ajsl134', 'c4', function(err){
+    if(err) alert()
+});
+
